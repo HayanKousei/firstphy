99424bf24e69890a521282d23cfa751ceff07add
HayanKousei <darielbatista769@gmail.com>
2022-09-21 20:52:01 -0400

    added sort

--- lists.py
+++ lists.py
@@ -1,1 +1,10 @@
+# list of names
 names = ["Dariel", "Dandan", "Luffy", "Ace"]
+
+names.append("Drake")
+# Append is to add a value"
+
+names.sort()
+#sorts names in alphabetical order
+
+print(names)
